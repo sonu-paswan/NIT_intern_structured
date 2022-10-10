7104ce7ed52683c6ee98d62edb7d1b1a2c72f32e
Sonu Paswan <sonupas2806@gmail.com>
2022-10-10 17:53:38 +0530

    1st commit 101022

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 
 const express=require('express');
+const { model, Schema } = require('mongoose');
 const App=express();
+const mongoose=require('mongoose',{});
 App.set('view engine','ejs');
 const port=1000;
 App.use(express.urlencoded({extended:true}));
 App.use(express.static("public"));
+
+// mongoose connection
+mongoose.connect('mongodb://localhost:27017/department');
+const dSchema=Schema({
+    id:Number,
+    name:String,
+    about:String,
+    faculty:Array
+})
+const dModel=mongoose.model('department',dSchema);
+
 
 //front page and home
 App.get('/',(req,res)=>{
@@ -40,12 +53,28 @@ console.log(req.body);
 //department
 App.get('/department/:name',(req,res)=>{
     let {name}=req.params;
-    res.render("department");
+
+    dModel.find({'id':name},(err,docs)=>{
+        if(err) console.log(err);
+        else{
+            // console.log(docs[0]);
+            res.render("department",{depart:docs[0]});
+        }
+    })
 })
 
 // faculty and courses
-App.get('/faculty',(req,res)=>{
-    res.render("faculty");
+let arr=['','Basic and Applied Science','Chemical Engineering','Civil Engineering','Computer Science and Engineering','Electrical Engineering','Electronic and Communication Engineering','Mechanical Engineering'];
+App.get('/faculty/:id',(req,res)=>{
+    // res.render("faculty");
+    let {id}=req.params;
+    dModel.find({'id':id},(err,docs)=>{
+        if(err) console.log(err);
+        else{
+            // console.log(docs[0].faculty);
+            res.render("faculty",{faculty:docs[0].faculty,depart:arr[id]});
+        }
+    })
 })
 App.get('/courses',(req,res)=>{
     res.render("courses");
